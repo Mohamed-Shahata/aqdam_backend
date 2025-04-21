065abd744bd22e105111baf273a3e848cd6363ca
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 18:13:59 -0100

    update

--- db/data-source.ts
+++ db/data-source.ts
@@ -11,7 +11,7 @@ export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
   url: process.env.DATABASE_URI,
   entities: [User, Post, Job, Reaction, Notification],
-  migrations: ['db/migrations/*.ts']
+  migrations: ['dist/db/migrations/*.js']
 }
 
 const dataSource = new DataSource(dataSourceOptions);
