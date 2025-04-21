19691230d924f65cb34ab9f1ae0b8798c9dfb0e1
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 12:18:24 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix("api")
+  app.setGlobalPrefix("api");
 
   app.enableCors({
-    origin: '*'
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe({
-    forbidNonWhitelisted: true,
-    whitelist: true,
-    transform: true
-  }))
 
-  await app.listen(5000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
