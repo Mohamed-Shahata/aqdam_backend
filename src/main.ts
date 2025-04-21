aa9c6d25a442ddcb232b64b9de36bba13f98a530
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 21:07:41 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as compression from "compression"
+// import * as compression from "compression"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api");
 
-  app.use(compression())
+  // app.use(compression())
   app.enableCors({
     origin: '*',  // يسمح لكل الدومينات
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],  // يحدد الطرق المسموح بها
