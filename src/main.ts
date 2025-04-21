eed4dd873d0df31f36710c04ff9495bd7956096a
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 11:43:47 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api")
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     forbidNonWhitelisted: true,
