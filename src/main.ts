12455b8b00b981b3f90efaa6c953a4da6e7bb369
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 19:00:24 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix("api");
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'PATCH', 'POST', 'DELETE', 'PUT'],
     credential: true
   });
