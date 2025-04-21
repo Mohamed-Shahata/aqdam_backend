eadd432c5d287916aabf61196ca2b051301974ee
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 10:17:06 -0100

    update

--- src/modules/redis/redis.service.ts
+++ src/modules/redis/redis.service.ts
@@ -8,20 +8,19 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   private subscriber: Redis;
 
   constructor(private config: ConfigService) {
-    const redisOptions = {
-      host: this.config.get<string>("REDIS_HOST"),
-      // port: this.config.get<number>("REDIS_PORT"),
+    const redisUrl = this.config.get<string>('REDIS_URL');
 
-    };
+    if (!redisUrl) {
+      throw new Error('REDIS_URL is not defined in .env file');
+    }
 
-    this.client = new Redis(redisOptions);
-    this.subscriber = new Redis(redisOptions);
+    this.client = new Redis(redisUrl);
+    this.subscriber = new Redis(redisUrl);
   }
 
   async onModuleInit() {
     await this.client.ping();
     await this.subscriber.ping();
-    console.log("Connected to Redis");
   }
 
   async onModuleDestroy() {
@@ -69,14 +68,14 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   async deleteByPattern(pattern: string): Promise<void> {
     const stream = this.client.scanStream({
       match: pattern,
-      count: 100,
+      count: 100, // عدد المفاتيح اللي بتتفحص في كل دفعة
     });
 
     return new Promise<void>((resolve, reject) => {
       stream.on("data", async (keys: string[]) => {
         if (keys.length > 0) {
           try {
-
+            // حذف المفاتيح باستخدام DEL
             await this.client.del(...keys);
           } catch (error) {
             console.error(`Error deleting keys for pattern ${pattern}:`, error);
@@ -86,12 +85,12 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
       });
 
       stream.on("end", () => {
-
+        // الـ scan خلّص
         resolve();
       });
 
       stream.on("error", (error) => {
-
+        // معالجة أي أخطاء أثناء الـ scan
         console.error(`Error scanning keys for pattern ${pattern}:`, error);
         reject(error);
       });
