6a0862b876eb17daf8747a23b5bb8e77d53f5bee
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 18:18:57 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from "compression"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(compression());
 
   await app.listen(5000);
 }
