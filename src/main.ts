d520674e1fbaca43183bc3f3f71217f4457a4bce
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-21 22:48:07 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   app.setGlobalPrefix("api");
 
   app.enableCors({
-    origin: '*'
+    origin: ['http://localhost:3000', 'https://aqdamfrontend.vercel.app'], // ضيف رابط الفرونت لو عندك
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   app.useGlobalPipes(
