e49fc3209f7ffcc77ca9514748d1f993e5e65c86
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-22 20:25:49 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: 'Content-Type, Authorization'
   });
 
   app.useGlobalPipes(
