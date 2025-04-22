3534606876667f57c3f5b1f9bd264dcd8f74635a
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-22 20:33:38 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-    Credential: true
+    credential: true
   });
 
   app.useGlobalPipes(
