63e87c7c1738e8e9a8fa56b193a9a3acd595a816
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-22 19:40:15 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from "compression"
-import * as cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,30 +11,8 @@ async function bootstrap() {
   app.use(compression())
   app.enableCors({
     origin: '*',
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
   });
-
-  // app.use(cors({
-  //   origin: 'https://aqdem-git-aqdam-mohameds-projects-f5551999.vercel.app',
-  //   methods: "GET, POST, PUT, PATCH, DELETE",
-  //   credentials: true
-  // }));
-
-
-  // app.use((req, res, next) => {
-  //   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // نفس ال origin اللي في cors
-  //   // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501"); // نفس ال origin اللي في cors
-  //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  //   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  //   res.header("Access-Control-Allow-Credentials", "true");
-
-  //   if (req.method === "OPTIONS") {
-  //     return res.sendStatus(200);
-  //   }
-
-  //   next();
-  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
