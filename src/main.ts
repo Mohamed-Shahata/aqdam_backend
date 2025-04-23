caee1aa19eeeccd9cc53f996f44ca861403e67dd
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-23 10:28:41 -0100

    update

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.use(compression())
   app.enableCors({
-    origin: 'https://aqdem-git-aqdam-mohameds-projects-f5551999.vercel.app',
+    origin: 'https://aqdam-git-aqdam-mohameds-projects-f5551999.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true // لو بتستخدم JWT أو cookies
   });
