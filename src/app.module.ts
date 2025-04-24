1b7dd911bbf5f6d16b5fc1f83c98e2469d94c579
Mohamed-Shahata <mohamedmrslan@gmail.com>
2025-04-24 13:48:16 -0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { PostModule } from './modules/posts/post.module';
 import { JobModule } from './modules/jobs/job.module';
 import { NotificationModule } from './modules/notifications/notification.module';
 import { dataSourceOptions } from 'db/data-source';
-// import { ScheduleModule } from '@nestjs/schedule';s
+import { ScheduleModule } from '@nestjs/schedule'; s
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 
@@ -15,6 +15,7 @@ import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     AuthModule,
+    ScheduleModule.forRoot(),
     UserModule,
     PostModule,
     JobModule,
